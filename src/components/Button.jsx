ea40d1ac96cae8f219ed1c84d81373b19f3f8261
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-01-18 18:29:06 +0800

    add stats, business, button, billing, and cardDeal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px] transition-transform duration-300 hover:translate-y-[-3px] active:scale-95 active:translate-y-0 ${styles}`}
+    >
+      Get Started
+    </button>
+  );
+};
 
-export default Button
+export default Button;
