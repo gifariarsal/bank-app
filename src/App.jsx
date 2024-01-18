48d922d26224182c49aff6e1769a800bb76a403d
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-01-18 11:04:15 +0800

    add: components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,50 @@
-import styles from './style'
+import React from "react";
+import styles from "./style";
+import {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTAButton,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
 
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
+    <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          stats
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTAButton />
+          <Footer />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
